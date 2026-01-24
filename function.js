1641c3076d16ac7c2cddf3b01fd20559ae9e0c31
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-01-24 16:46:42 +0100

    add loop for 5 rounds, add autostart

--- function.js
+++ function.js
@@ -37,5 +37,23 @@ function playRound(humanChoice, computerChoice) {
 
 }
 
+function playGame() {
+    for (let rounds = 0; rounds < 5; rounds++ ) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice()
 
+        console.log(playRound(humanChoice, computerChoice));
+        console.log(`Score  Human: ${HumanScore}, Computer: ${ComputerScore}`)
+    }
+
+
+if (HumanScore > ComputerScore) {
+    console.log("You are the overall winner!");
+} else if (ComputerScore > HumanScore) {
+    console.log("You lose the game!"); 
+} else {
+    console.log("It's a draw!");
+}
+}
+playGame();
 
