ebc4cb0a1a14deaf6a6f8d19feccdbff41a90c63
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-01-24 16:39:32 +0100

    add human choice, add game, game conditions

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
-console.log("Hello World")
+let HumanScore = 0
+let ComputerScore = 0
 
 function getComputerChoice() {
    let number = Math.floor(Math.random() * 3);
@@ -11,5 +12,30 @@ function getComputerChoice() {
         return "paper"; 
     }
 }
+
+function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissors?")
+    return(choice)
+}
+
+function playRound(humanChoice, computerChoice) {
+     if ( 
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+     ) {
+        HumanScore++    
+        return (`You win ${humanChoice} beats ${computerChoice}.`)                
+    } 
+    else if (humanChoice === computerChoice) {
+        return ("It's a draw!") 
+    } 
+    else {
+        ComputerScore++
+        return (`You lose! ${computerChoice} beats ${humanChoice}`)
+    }           
+
+}
+
 
 
